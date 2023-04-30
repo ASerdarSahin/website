7ad3fce598e869d7b0d88d3aefa5ec3d2e46dfe3
User2 <aserdarsahin@outlook.com>
2023-04-30 23:48:13 +0300

    create database

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# New MySQL DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:KH5!ajwQQ72uxrh@localhost/my_db'
 
 @app.route('/')
 def hello_world():  # put application's code here
