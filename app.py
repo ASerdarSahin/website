f56f94c319c2c1c6dea8065c17a4b597b13a31c7
User2 <aserdarsahin@outlook.com>
2023-05-02 04:37:06 +0300

    add individual blog posts, whitespaces

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, EqualTo, Length
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms.widgets import TextArea
 
 # Create a Flask Instance
 app = Flask(__name__)
@@ -20,10 +21,29 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
+# Create a Blog Post Model
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    author = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    slug = db.Column(db.String(255))
+
+
+# Create a Posts Form Class
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    content = StringField("Content", validators=[DataRequired()], widget=TextArea())
+    author = StringField("Author", validators=[DataRequired()])
+    slug = StringField("Slug", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
 # Create a Model
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(25), nullable=False,)
+    name = db.Column(db.String(25), nullable=False, )
     email = db.Column(db.String(120), nullable=False, unique=True)
     favorite_color = db.Column(db.String(120))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
@@ -33,6 +53,7 @@ class Users(db.Model):
     @property
     def password(self):
         raise AttributeError('Password is not a readable attribute.')
+
     @password.setter
     def password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -50,7 +71,8 @@ class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
     favorite_color = StringField("Favorite Color")
-    password_hash = PasswordField('Password',validators=[DataRequired(), EqualTo('password_hash2', message='Passwords must match')])
+    password_hash = PasswordField('Password', validators=[DataRequired(),
+                                                          EqualTo('password_hash2', message='Passwords must match')])
     password_hash2 = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField("Submit")
 
@@ -67,6 +89,7 @@ class NamerForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
+
 # New MySQL DB
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:KH5!ajwQQ72uxrh@localhost/my_db'
 
@@ -74,6 +97,81 @@ class NamerForm(FlaskForm):
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('index.html')
+
+
+@app.route('/posts')
+def posts():
+    # Grab a list of posts from database
+    posts = BlogPost.query.order_by(BlogPost.date_posted)
+    return render_template('posts.html', posts=posts)
+
+
+@app.route('/posts/<int:id>')
+def post(id):
+    post = BlogPost.query.get_or_404(id)
+    return render_template('post.html', post=post)
+
+
+@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+def edit_post(id):
+    post = BlogPost.query.get_or_404(id)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.author = form.author.data
+        post.content = form.content.data
+        post.slug = form.slug.data
+        # Update the Database
+        # db.session.update(post)
+        db.session.add(post)
+        db.session.commit()
+        flash('Post has been updated')
+        return redirect(url_for('posts', id=post.id))
+    form.title.data = post.title
+    form.author.data = post.author
+    form.content.data = post.content
+    form.slug.data = post.slug
+    return render_template('edit_post.html', form=form)
+
+
+# Delete Post
+@app.route('/posts/delete/<int:id>')
+def delete_post(id):
+    post_to_delete = BlogPost.query.get_or_404(id)
+
+    try:
+        db.session.delete(post_to_delete)
+        db.session.commit()
+        flash('Post has been deleted')
+        posts = BlogPost.query.order_by(BlogPost.date_posted)
+        return render_template('posts.html', posts=posts)
+    except:
+        flash('There was an error deleting the post')
+        posts = BlogPost.query.order_by(BlogPost.date_posted)
+        return render_template('posts.html', posts=posts)
+
+
+# Add Post Page
+@app.route('/add-post', methods=['GET', 'POST'])
+def add_post():
+    form = PostForm()
+
+    if form.validate_on_submit():
+        # Add Data to the Database
+        post = BlogPost(title=form.title.data, content=form.content.data, author=form.author.data, slug=form.slug.data)
+        # Clear the Form
+        form.title.data = ''
+        form.content.data = ''
+        form.author.data = ''
+        form.slug.data = ''
+
+        # Add to the Database
+        db.session.add(post)
+        db.session.commit()
+
+        # Flash Message
+        flash('Blog Post Added')
+    return render_template('add_post.html', form=form)
 
 
 # Create a function to add data to the database
@@ -87,7 +185,8 @@ def add_user():
         if user is None:
             # Hash the password
             hashed_pw = generate_password_hash(form.password_hash.data, method="sha256")
-            user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data, password_hash=hashed_pw)
+            user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data,
+                         password_hash=hashed_pw)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
@@ -117,6 +216,7 @@ def delete(id):
         flash("There was a problem deleting data.")
         our_users = Users.query.order_by(Users.date_added)
         return render_template('add_user.html', form=form, name=name, our_users=our_users)
+
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
@@ -149,20 +249,20 @@ def test_pw():
 
     # Validate Form
     if form.validate_on_submit():
-        email=form.email.data
-        password=form.password_hash.data
+        email = form.email.data
+        password = form.password_hash.data
         # Clear the form
-        form.email.data=''
-        form.password_hash.data=''
+        form.email.data = ''
+        form.password_hash.data = ''
 
         # Lookup user by email address
         pw_to_check = Users.query.filter_by(email=email).first()
         # Check the password
-        passed=check_password_hash(pw_to_check.password_hash, password)
-
+        passed = check_password_hash(pw_to_check.password_hash, password)
 
         flash('Form Submitted Successfully!')
-    return render_template('test_pw.html', email=email, password=password, pw_to_check=pw_to_check, passed=passed, form=form)
+    return render_template('test_pw.html', email=email, password=password, pw_to_check=pw_to_check, passed=passed,
+                           form=form)
 
 
 @app.route('/name', methods=['GET', 'POST'])
@@ -180,20 +280,22 @@ def name():
 @app.route('/blog')
 def blog():
     posts = [{'title': 'Post 1', 'content': 'This is the content of post 1. Lorem ipsum dolor sit amet.'},
-            {'title': 'Post 2', 'content': 'This is the content of post 2. Lorem ipsum dolor sit amet.'}
-            ]
+             {'title': 'Post 2', 'content': 'This is the content of post 2. Lorem ipsum dolor sit amet.'}
+             ]
     return render_template('blog.html', sunny=True, author='Serdar', posts=posts)
 
 
 if __name__ == '__main__':
     app.run()
 
-#Invalid URL
+
+# Invalid URL
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-#Internal Server Error
+
+# Internal Server Error
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
